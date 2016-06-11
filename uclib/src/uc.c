f299730e2fd44edf069d76190d735348af0a3690
Tomasz Chadzynski <t.chadzynski@gmail.com>
2016-06-11 03:03:54 -0700

    Need the KDIR in simulation mode after all so even when compile in sim mode KDIR has to be passed
    Incorporated code from first version so far compiles only in sim mode

--- uclib/src/uc.c
+++ uclib/src/uc.c
@@ -1,7 +1,7 @@
 /*
  * uc.c
  * 
- * Temporary dummy uc library to satisfy daemon build requirements
+ * Interface library to the rover kernel driver
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -10,7 +10,25 @@
  * Copyright (C) 2016 Tomasz Chadzynski
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/input.h>
+
+#include <assert.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <err.h>
+
 #include "uc.h"
+#include "../config.h"
+
+/*
+ * TODO:
+ * use syslog for logging err for errors reporting to the console (may be)
+ * man 3 syslog, man 3 err
+ */
+
 
 struct adsensor* alloc_adsensor()
 {
@@ -18,6 +36,141 @@ struct adsensor* alloc_adsensor()
 	ret->initialized = 0;
 	return ret;
 }
+
+#ifndef SIM_MODE
+
+int init(struct adsensor * dev)
+{
+    int ret = 0;
+    if(dev->initialized) {
+        perror("Already initialized");
+        return -EPERM;
+    }
+
+    dev->dev_file = open(DRV_FILE_PATH, O_RDWR);
+    if(dev->dev_file < 0) {
+        ret = errno;
+        err(1,"%s","opening drv");
+        return -ret;
+    }
+
+    dev->event_file = open(DRV_POLL_PATH, O_RDONLY);
+    if(dev->event_file < 0) {
+        ret = errno;
+        err(1,"%s","opening event file");
+        //close the opened dev file first
+        if(close(dev->dev_file) == -1){
+            ret = errno;
+            err(1,"%s","closing drv");
+        }
+
+        return -ret;
+    }
+
+    dev->initialized = 1;
+    return 0;
+}
+
+int release(struct adsensor * dev) 
+{
+    int ret = 0;
+
+    if(dev->initialized) {
+        if(close(dev->dev_file) == -1){
+            ret = errno;
+            err(1,"%s","closing drv");
+        }
+
+        if(close(dev->event_file) == -1){
+            ret = errno;
+            err(1,"%s","closing event file");
+        }
+    }
+    free(dev);
+    
+    return -ret;
+}
+
+int send_ioctl_command( struct adsensor * dev, enum ioctl_command cmd, int16_t arg1, int16_t arg2)
+{
+    int ret=0;
+    int16_t args[2] = {arg1, arg2};
+    
+    if(ioctl(dev->dev_file, cmd, args) == -1) {
+        ret = errno;
+        err(1,"%s","ioctl command");
+    }
+
+    return -ret;
+}
+
+int get_wheels_state(struct adsensor *dev, struct uc0_wheel_state *device)
+{
+    int ret = 0;
+    ssize_t bytes_read = 0;
+    if(!dev->initialized){
+        perror("Cant read uninitialized resource");
+        return -EIO;
+    }
+
+    if(lseek(dev->dev_file, 0, SEEK_SET) == -1) {
+       ret = errno;
+      err(1,"%s","read wheel state seek");
+        return -ret;
+    }
+   
+    bytes_read = read(dev->dev_file, device, sizeof(struct uc0_wheel_state));
+    
+
+    if(bytes_read == -1) {
+        ret = errno;
+        err(1,"%s","read wheel state read");
+        return -ret;
+    }
+
+    return 0;
+}
+
+int read_event(struct adsensor *dev, struct input_event *ev)
+{
+    int ret =0;
+    ssize_t bytes_read = 0;
+
+    if(!dev->initialized){
+        perror("Cant read uninitialized resource");
+        return -EIO;
+    }
+
+    bytes_read = read(dev->event_file, ev, sizeof(struct input_event));
+
+    if(bytes_read == -1) {
+        ret = errno;
+        err(1,"%s","read event");
+        return -ret;
+    }
+
+    return 0;
+}
+
+int read_distance(struct adsensor *dev, int32_t *distance)
+{
+    int ret =0;
+    struct input_event ev;
+
+    ret = read_event(dev, &ev);
+
+    if(ret < 0) {
+        return ret;
+    }
+
+    if(ev.type == EV_MSC || ev.code == MSC_RAW) {
+        *distance = ev.value;
+    }
+
+    return 0;
+}
+
+#else //bridge in simulation mode 
 
 int init(struct adsensor * dev)
 {
@@ -41,7 +194,7 @@ int send_ioctl_command( struct adsensor * dev, enum ioctl_command cmd, int16_t a
     return 0;
 }
 
-int get_wheels_state(struct adsensor *dev, struct device_state *device)
+int get_wheels_state(struct adsensor *dev, struct uc0_wheel_state *device)
 {
     device->left_wheel_speed = 0;
     device->right_wheel_speed = 0;
@@ -53,6 +206,13 @@ int get_wheels_state(struct adsensor *dev, struct device_state *device)
     device->right_wheel_speed,
     device->wheel_max_speed,
     device->wheel_min_speed);
+
+    return 0;
+}
+
+int read_event(struct adsensor *dev, struct input_event *ev)
+{
+    printf("SIM: read_event\n");
 
     return 0;
 }
@@ -77,3 +237,4 @@ int read_distance(struct adsensor *dev, int32_t *distance)
     return 0;
 }
 
+#endif
--- uclib/src/uc.h
+++ uclib/src/uc.h
@@ -1,7 +1,7 @@
 /*
  * uc.h
  * 
- * Temporary dummy uc library to satisfy daemon build requirements
+ * Interface library to the rover kernel driver
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -10,19 +10,12 @@
  * Copyright (C) 2016 Tomasz Chadzynski
  */
 
-#ifndef __UC_
-#define __UC_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <assert.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <err.h>
+#ifndef __UC_H_
+#define __UC_H_
 
 #include <stdint.h>
-#include "types.h"
+#include <rover.h>
+
 
 struct adsensor {
     int initialized;
@@ -38,7 +31,7 @@ struct adsensor* alloc_adsensor();
 int init(struct adsensor * dev);
 int release(struct adsensor * dev);
 int send_ioctl_command( struct adsensor * dev, enum ioctl_command cmd, int16_t arg1, int16_t arg2);
-int get_wheels_state(struct adsensor *dev, struct device_state *device);
+int get_wheels_state(struct adsensor *dev, struct uc0_wheel_state *device);
 int read_distance(struct adsensor *dev, int32_t *distance);
 
 #ifdef __cplusplus
